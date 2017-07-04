c184bf75123496e5cb650b6e9dbaf474cc361182
Maksim <maksim.shkarbaliuk@sotsos.com>
2017-07-04 17:03:31 +0300

    Init commit

---
+++ app/src/main/java/ru/chemaxa/myapplication/MainActivity.kt
@@ -0,0 +1,34 @@
+package ru.chemaxa.myapplication
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View; // подключаем класс View для обработки нажатия кнопки
+import android.widget.EditText; // подключаем класс EditText
+
+
+class MainActivity : AppCompatActivity() {
+    val EXTRA_MESSAGE = "EXTRA_MESSAGE"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+        // действия, совершаемые после нажатия на кнопку
+        // Создаем объект Intent для вызова новой Activity
+        val intent = Intent(this, DisplayMessageActivity::class.java)
+        // Получаем текстовое поле в текущей Activity
+        val editText = findViewById(R.id.edit_message) as EditText
+        // Получае текст данного текстового поля
+        val message = editText.text.toString()
+        // Добавляем с помощью свойства putExtra объект - первый параметр - ключ,
+        // второй параметр - значение этого объекта
+        intent.putExtra(EXTRA_MESSAGE, message)
+        // запуск activity
+        startActivity(intent)
+    }
+
+}
