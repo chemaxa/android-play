94f781f0e493ed2872a5f596d3acd4d8233fa4c3
Maksim <maksim.shkarbaliuk@sotsos.com>
2017-07-05 17:20:32 +0300

    Added new activity

--- app/src/main/java/ru/chemaxa/myapplication/MainActivity.kt
+++ app/src/main/java/ru/chemaxa/myapplication/MainActivity.kt
@@ -1,36 +1,93 @@
 package ru.chemaxa.myapplication
 
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.util.Log
-import android.view.View; // подключаем класс View для обработки нажатия кнопки
-import android.widget.EditText; // подключаем класс EditText
-
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        val EXTRA_MESSAGE = "EXTRA_MESSAGE"
-    }
+    var resultField: TextView? = null // текстовое поле для вывода результата
+    var operand: Double = 0.0  // операнд операции
+    var lastOperation = "=" // последняя операция
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // получаем все поля по id из activity_main.xml
+        resultField = findViewById(R.id.resultField) as TextView
     }
 
-    fun sendMessage(view: View) {
-        // действия, совершаемые после нажатия на кнопку
-        // Создаем объект Intent для вызова новой Activity
-        val intent = Intent(this, DisplayMessageActivity::class.java)
-        // Получаем текстовое поле в текущей Activity
-        val editText = findViewById(R.id.edit_message) as EditText
-        // Получае текст данного текстового поля
-        val message = editText.text.toString()
-        // Добавляем с помощью свойства putExtra объект - первый параметр - ключ,
-        // второй параметр - значение этого объекта
-        intent.putExtra(EXTRA_MESSAGE, message)
-        // запуск activity
-        startActivity(intent)
+    // сохранение состояния
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString("OPERATION", lastOperation)
+        if (operand != 0.0)
+            outState.putDouble("OPERAND", operand)
+        super.onSaveInstanceState(outState)
     }
 
+    // получение ранее сохраненного состояния
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        lastOperation = savedInstanceState.getString("OPERATION")
+        operand = savedInstanceState.getDouble("OPERAND")
+        resultField?.text = operand.toString()
+        resultField?.text = lastOperation
+    }
+
+    // обработка нажатия на числовую кнопку
+    fun onNumber(view: View) {
+
+        val button = view as Button
+        resultField?.append(button.getText())
+
+        if (lastOperation == "=" && operand != 0.0) {
+            operand = 0.0
+        }
+    }
+
+    // обработка нажатия на кнопку операции
+    fun onOperationClick(view: View) {
+
+        val button = view as Button
+        val op = button.getText().toString()
+        var number = resultField?.text.toString()
+        // если введенно что-нибудь
+        if (number.length > 0) {
+            number = number.replace(',', '.')
+            try {
+                performOperation(java.lang.Double.valueOf(number), op)
+            } catch (ex: NumberFormatException) {
+                resultField?.setText("")
+            }
+
+        }
+        lastOperation = op
+        resultField?.text = lastOperation
+    }
+
+    private fun performOperation(number: Double, operation: String) {
+
+        // если операнд ранее не был установлен (при вводе самой первой операции)
+        if (operand == 0.0) {
+            operand = number
+        } else {
+            if (lastOperation == "=") {
+                lastOperation = operation
+            }
+            when (lastOperation) {
+                "=" -> operand = number
+                "/" -> if (number == 0.0) {
+                        operand = 0.0
+                    } else {
+                        operand /= number
+                    }
+                "*" -> operand *= number
+                "+" -> operand += number
+                "-" -> operand -= number
+            }
+        }
+        resultField?.text = operand.toString().replace('.', ',')
+        resultField?.setText("")
+    }
 }
