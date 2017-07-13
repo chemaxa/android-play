97261cb509eaf8c8fde19433210250cea000beeb
Maksim <maksim.shkarbaliuk@sotsos.com>
2017-07-13 15:29:58 +0300

    Rewrite some logic Added new input field

--- app/src/main/java/ru/chemaxa/myapplication/MainActivity.kt
+++ app/src/main/java/ru/chemaxa/myapplication/MainActivity.kt
@@ -1,14 +1,16 @@
 package ru.chemaxa.myapplication
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    var resultField: TextView? = null // текстовое поле для вывода результата
+    lateinit var resultField: TextView // текстовое поле для вывода результата
+    lateinit var inputField: TextView // текстовое поле для вывода введенных символов
     var operand: Double? = null  // операнд операции
     var lastOperation = "=" // последняя операция
 
@@ -17,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // получаем все поля по id из activity_main.xml
         resultField = findViewById(R.id.resultField) as TextView
+        inputField = findViewById(R.id.inputField) as TextView
     }
 
     // сохранение состояния
@@ -33,64 +36,52 @@ class MainActivity : AppCompatActivity() {
         super.onRestoreInstanceState(savedInstanceState)
         lastOperation = savedInstanceState.getString("OPERATION")
         operand = savedInstanceState.getDouble("OPERAND")
-        resultField?.text = operand.toString()
-        resultField?.text = lastOperation
+        resultField.text = operand.toString()
+        resultField.text = lastOperation
     }
 
     // обработка нажатия на числовую кнопку
     fun onNumber(view: View) {
-
         val button = view as Button
-        resultField?.append(button.getText())
-
-        if (lastOperation == "=" && operand != null) {
-            operand = null
+        //resultField?.append(button.text)
+        if (operand == null) {
+            operand = button.text.toString().toDouble()
         }
+        resultField.text = resultField.text.toString() + performOperation(operand, lastOperation).toString()
     }
 
     // обработка нажатия на кнопку операции
-    fun onOperationClick(view: View) {
+    fun onOperation(view: View) {
         val button = view as Button
-        val op = button.getText().toString()
-        var number = resultField?.text.toString()
-        // если введенно что-нибудь
-        if (number.isNotEmpty()) {
-            number = number.replace(',', '.')
-            try {
-                performOperation(java.lang.Double.valueOf(number), op)
-            } catch (ex: NumberFormatException) {
-                resultField?.setText("")
-            }
-
-        }
+        val op = button.text.toString()
         lastOperation = op
-        resultField?.text = lastOperation
+        resultField.text = resultField.text.toString() + lastOperation
     }
 
-    private fun performOperation(number: Double?, operation: String) {
-        // если операнд ранее не был установлен (при вводе самой первой операции)
-        if (operand == null) {
-            operand = number
-        } else {
-            if (number != null) {
-                if (lastOperation == "=") {
-                    lastOperation = operation
+    private fun updateInput(data: String) {
+        inputField.text = inputField.text.toString() + data
+    }
+    private fun updateResult(result: String) {
+        resultField.text = resultField.text.toString() + result
+    }
+
+    private fun performOperation(number: Double?, operation: String): Double {
+        Log.d("Operation", operation)
+        Log.d("Number", number.toString())
+        Log.d("Operand", operand.toString())
+        if (number != null) {
+            when (operation) {
+                "=" -> operand = number
+                "/" -> if (number == 0.toDouble()) {
+                    operand = 0.0
+                } else {
+                    operand = operand!! / number
                 }
-                when (lastOperation) {
-                    "=" -> operand = number
-                    "/" -> if (number == 0.toDouble()) {
-                        operand = 0.0
-                    } else {
-                        operand = operand!! / number
-                    }
-                    "*" -> operand = operand!! * number
-                    "+" -> operand = operand!! + number
-                    "-" -> operand = operand!! -number
-                }
+                "*" -> operand = operand!! * number
+                "+" -> operand = operand!! + number
+                "-" -> operand = operand!! - number
             }
         }
-
-        resultField?.text = operand.toString().replace('.', ',')
-        resultField?.setText("")
+        return operand!!
     }
 }
